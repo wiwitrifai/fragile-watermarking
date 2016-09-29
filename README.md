c39670036f910c46a145e86a7d718606cbbc1108
Wiwit Rifa'i <wiwitrifai@gmail.com>
2016-09-29 13:39:58 +0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Fragile Watermarking Implementation for IF4020 Crypthograpy Assignment
+
+## Assignment Specification
+http://informatika.stei.itb.ac.id/~rinaldi.munir/Kriptografi/2016-2017/Tubes1-Kripto-2016.doc
+
+## Reference
+A Chaos-based Fragile Watermarking Method in Spatial Domain for Image Authentication
+http://informatika.stei.itb.ac.id/~rinaldi.munir/Kriptografi/2016-2017/Makalah_ISITIA_2015.pdf
---
+++ colorcount.py
@@ -0,0 +1,24 @@
+import sys
+from PIL import Image
+
+d = dict()
+colorsum = 0
+
+image = Image.open(str(sys.argv[1]))
+
+print("Counting...")
+
+for i in range(image.size[0]):
+    for j in range(image.size[1]):
+        r, g, b = image.getpixel((i,j))
+        key = "R: " + str(r).zfill(3) + " G: " + str(g).zfill(3) + " B: " + str(b).zfill(3) 
+        if key in d:
+            d[key] += 1
+        else:
+            d[key] = 1
+            colorsum += 1
+
+for key in d:
+    print("Color: ", key, " Occurence: ", d[key])
+print("Total color: ", colorsum)
+
