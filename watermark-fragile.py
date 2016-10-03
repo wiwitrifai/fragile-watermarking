56dca79fd18f9f552fd697308a2c1ea47558f9c6
Wiwit Rifa'i <wiwitrifai@gmail.com>
2016-10-03 18:22:48 +0700

    add insert + extract watermark

--- watermark-fragile.py
+++ watermark-fragile.py
@@ -1,24 +1,39 @@
 import sys
 from PIL import Image
 
-d = dict()
-colorsum = 0
+def extract_lsb(inputpath, outputpath):
+    cover = Image.open(inputpath)
+    lsb = Image.new("1", cover.size);
+    px_cover = cover.load()
+    px_lsb = lsb.load()
 
-image = Image.open(str(sys.argv[1]))
+    for i in range(cover.width):
+        for j in range(cover.height):
+            p = px_cover[i, j]
+            px_lsb[i, j] = (p[0] & 1)
+    lsb.save(outputpath);
+    lsb.show();
 
-print("Counting...")
+def insert_lsb(inputpath, watermarkpath, outputpath):
+    cover = Image.open(inputpath)
+    watermark = Image.open(watermarkpath).convert("1")
+    output = Image.new(cover.mode, cover.size)
+    px_cover = cover.load()
+    px_watermark = watermark.load()
+    px_output = output.load()
 
-for i in range(image.size[0]):
-    for j in range(image.size[1]):
-        r, g, b = image.getpixel((i,j))
-        key = "R: " + str(r).zfill(3) + " G: " + str(g).zfill(3) + " B: " + str(b).zfill(3) 
-        if key in d:
-            d[key] += 1
-        else:
-            d[key] = 1
-            colorsum += 1
+    for i in range(cover.width):
+        for j in range(cover.height):
+            p = list(px_cover[i, j])
+            p[0] = (p[0] & 0b11111110) | (px_watermark[i % watermark.width, j % watermark.height] & 1)
+            px_output[i, j] = tuple(p)
+    output.save(outputpath)
+    output.show()
 
-for key in d:
-    print("Color: ", key, " Occurence: ", d[key])
 
-print("Total color: ", colorsum)
+def main():
+    insert_lsb(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
+    extract_lsb(str(sys.argv[3]), "extracted_lsb_"+str(sys.argv[3]))
+
+if __name__ == '__main__':
+  main()
